c25f039bfb36f510f3308ef9e39135f75899d60e
alexistec <alexisquionez21@gmail.com>
2017-06-28 01:47:56 -0300

    Edit readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 <p align="center">
   <img src="logo.png">
 </p>
+<hr>
+<h2>DASHBOARD</h2>
+<a href="http://192.99.247.215/ecobicix/" target="_blank">VER DEMO</a>
--- api/models/usuario_model.php
+++ api/models/usuario_model.php
@@ -11,12 +11,15 @@ class Usuario_model extends DatabaseMysqlConfig{
 
     public $mensaje;
 
+    //todos los usuarios registrados
     public function getDataUsers(){
       $this->query = 'select * from usuarios';
       $this->get_results_from_query();
       return $this->rows;
     }
 
+
+    //Un usuario especifico ID
     public function getDataUserId($idUsuario){
 
       $this->query = "SELECT persona.nombre,persona.apellido,persona.direccion,usuarios.usuarios,usuarios.idusuarios
@@ -29,7 +32,7 @@ class Usuario_model extends DatabaseMysqlConfig{
       if(count($this->rows) ==2){
         print_r(json_encode($this->rows));
       }else{
-        print_r(json_encode(Array("Mensaje"=>"No datos")));
+        print_r(json_encode(Array("Mensaje"=>"No existe el usuario con el id ".$idUsuario)));
       }
     }
 
